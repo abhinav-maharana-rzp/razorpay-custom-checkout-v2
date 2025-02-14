42c5cc996ffb31d24a848365ed3a3ef52764e9c1
Abhinav Maharana <abhinav.maharana@razorpay.com>
2025-02-14 10:28:30 +0530

    fixes

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://razorpay-custom-checkout-v2-production.up.railway.app/api";
 
 // Function to display the modal
 function showModal() {
